5353a030c2b4d3c5a9d032992539528cc09bb7f6
Cédric Raud <cedric@clearwage.com>
2020-09-24 18:14:17 +0200

    Disable zoom on iOS

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     title: 'Ça vaut le coup ?',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Un calculateur de temps gagné pour comprendre la puissance des effets cumulés.' }
     ],
     link: [
